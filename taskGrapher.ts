17ee6181816e598afc93b9edb2f66b94a29669a2
Dominik <dominik@lameter.com>
2023-12-28 15:11:47 -0600

    add super basic indentation hierachy building. Still need to handle gaps between tasks probably

--- taskGrapher.ts
+++ taskGrapher.ts
@@ -1,3 +1,4 @@
+import { indentationChecker } from "./indentationCounter.ts";
 import { Task, parseTask } from "./taskParser.ts"
 
 export interface TaskGraph {
@@ -8,10 +9,39 @@ export interface TaskGraph {
 export function textToTaskGraph(text: string): TaskGraph[] {
     const results: TaskGraph[] = []
 
+    let lastIndentation = 0
+    const taskStack: TaskGraph[] = []
     for (const line of text.split('\n')) {
         const task = parseTask(line)
         if (task) {
-            results.push({ task: task })
+            const indentation = indentationChecker(line)
+            const validMember = { task: task }
+
+            if (indentation === lastIndentation && taskStack.length > 0) {
+                taskStack.pop()
+            } else if (indentation < lastIndentation) {
+                for (let i = 0; i < lastIndentation - indentation + 1; i++) {
+                    taskStack.pop()
+                }
+            } else if (indentation === lastIndentation + 1 || indentation === lastIndentation) {
+                // do nothing
+            } else {
+                throw new Error('task list jumped too far forward!')
+            }
+            lastIndentation = indentation
+
+            if (taskStack.length > 0) {
+                const parent = taskStack[taskStack.length - 1]
+                if (parent?.children) {
+                    parent.children.push(validMember)
+                } else {
+                    parent.children = [validMember]
+                }
+            } else {
+                results.push(validMember)
+            }
+
+            taskStack.push(validMember)
         }
     }
 
--- taskGrapherTest.ts
+++ taskGrapherTest.ts
@@ -12,16 +12,17 @@ Deno.test('taskGrapher',  async (t) => {
 
     await t.step('returns empty list if file has no checkboxes', () => {
         verifyParsedFileReturns(
-            `# Start of the file
-            here is a bunch of text!
+`# Start of the file
+here is a bunch of text!
 
-            [[A link to another file!]]
+[[A link to another file!]]
 
-            [A link to a web-site](https://localhost:3000)
+[A link to a web-site](https://localhost:3000)
 
-            ## Another header
-            this is some text under the header
-            `, [])
+## Another header
+this is some text under the header
+`, 
+            [])
     })
 
     await t.step('returns a single task if file is one checkbox', () => {
@@ -30,25 +31,25 @@ Deno.test('taskGrapher',  async (t) => {
 
     await t.step('returns a single task if file has one checkbox', () => {
         verifyParsedFileReturns(
-            `# Start of the file
-            here is a bunch of text!
+`# Start of the file
+here is a bunch of text!
 
-            [[A link to another file!]]
+[[A link to another file!]]
 
-            [A link to a web-site](https://localhost:3000)
+[A link to a web-site](https://localhost:3000)
 
-            - [ ] This is a task!
+- [ ] This is a task!
 
-            ## Another header
-            this is some text under the header
-            `, [{ task: { checked: false, text: 'This is a task!' } }])
+## Another header
+this is some text under the header`, 
+            [{ task: { checked: false, text: 'This is a task!' } }])
     })
 
     await t.step('returns multiple tasks if file is a few checkboxes', () => {
         verifyParsedFileReturns(
-            `- [ ] unchecked
-            - [x] checked 
-            - [ ] we have numbers, 142.34`,
+`- [ ] unchecked
+- [x] checked 
+- [ ] we have numbers, 142.34`,
             [
                 { task: { checked: false, text: 'unchecked' } },
                 { task: { checked: true, text: 'checked' } },
@@ -59,20 +60,85 @@ Deno.test('taskGrapher',  async (t) => {
 
     await t.step('returns multiple tasks if file has a few checkboxes spread throughout', () => {
         verifyParsedFileReturns(
-            `# Todo
-            Here is what I need to do:
-            - [ ] unchecked
+`# Todo
+Here is what I need to do:
+- [ ] unchecked
 
-            ## Project setup
-            - [x] checked 
-            Good thing I got that done
+## Project setup
+- [x] checked 
+Good thing I got that done
 
-            Miscellaneous
-            - [ ] we have numbers, 142.34`,
+Miscellaneous
+- [ ] we have numbers, 142.34`,
             [
                 { task: { checked: false, text: 'unchecked' } },
                 { task: { checked: true, text: 'checked' } },
                 { task: { checked: false, text: 'we have numbers, 142.34' } }
+            ]
+        )
+    })
+
+    await t.step('returns indented tasks', () => {
+        verifyParsedFileReturns(
+`- [ ] unchecked
+\t- [x] checked 
+\t\t- [ ] we have numbers, 142.34`,
+            [
+                { task: { checked: false, text: 'unchecked' }, children: [
+                    { task: { checked: true, text: 'checked' }, children: [
+                        { task: { checked: false, text: 'we have numbers, 142.34' } }
+                    ] }
+                ] }
+            ]
+        )
+    })
+
+    await t.step('returns indented tasks that go back and forth', () => {
+        verifyParsedFileReturns(
+`- [ ] unchecked
+\t- [x] checked 
+\t\t- [ ] we have numbers, 142.34
+\t- [x] checked 
+- [ ] unchecked`,
+            [
+                {
+                    task: { checked: false, text: 'unchecked' },
+                    children: [
+                        {
+                            task: { checked: true, text: 'checked' },
+                            children: [
+                                { task: { checked: false, text: 'we have numbers, 142.34' } }
+                            ]
+                        },
+                        {
+                            task: { checked: true, text: 'checked' }
+                        }
+                    ]
+                },
+                { task: { checked: false, text: 'unchecked' } }
+            ]
+        )
+    })
+
+    await t.step('returns indented tasks that jump back to the start', () => {
+        verifyParsedFileReturns(
+`- [ ] unchecked
+\t- [x] checked 
+\t\t- [ ] we have numbers, 142.34
+- [x] checked`,
+            [
+                {
+                    task: { checked: false, text: 'unchecked' },
+                    children: [
+                        {
+                            task: { checked: true, text: 'checked' },
+                            children: [
+                                { task: { checked: false, text: 'we have numbers, 142.34' } }
+                            ]
+                        }
+                    ]
+                },
+                { task: { checked: true, text: 'checked' } }
             ]
         )
     })
