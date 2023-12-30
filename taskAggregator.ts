8c0aeff4c9647a5683a7eacb8563f02abb46e3f2
Dominik <dominik@lameter.com>
2023-12-29 18:38:04 -0600

    build out task aggregator and it's tests

---
+++ taskAggregator.ts
@@ -0,0 +1,52 @@
+import { TaskGraph } from "./models/TaskGraph.ts";
+
+export interface TaskAggregate {
+    [text: string]: {
+        numChecked: number
+        numUnchecked: number
+        children?: TaskAggregate
+    }
+}
+
+export class TaskAggregator {
+    result: TaskAggregate = {}
+
+    addTaskGraphs(taskGraphs: TaskGraph[]) {
+        this.result = this.taskGraphsToTaskAggregate(taskGraphs, this.result)
+    }
+
+    getResult(): TaskAggregate {
+        return this.result
+    }
+
+    private taskGraphsToTaskAggregate(taskGraphs: TaskGraph[], taskAggregate: TaskAggregate = {}): TaskAggregate {
+        for (const taskGraph of taskGraphs) {
+            const task = taskGraph.task
+            if (taskAggregate[task.text]) {
+                if (taskGraph.task.checked) {
+                    taskAggregate[task.text].numChecked++
+                } else {
+                    taskAggregate[task.text].numUnchecked++
+                }
+            } else {
+                taskAggregate[task.text] = {
+                    numChecked: taskGraph.task.checked ? 1 : 0,
+                    numUnchecked: taskGraph.task.checked ? 0 : 1
+                }
+            }
+
+            if (taskGraph.children && taskGraph.children.length > 0) {
+                let childAggregate: TaskAggregate
+                if (taskAggregate[task.text].children) {
+                    childAggregate = this.taskGraphsToTaskAggregate(taskGraph.children, taskAggregate[task.text].children)
+                } else {
+                    childAggregate = this.taskGraphsToTaskAggregate(taskGraph.children)
+                }
+
+                taskAggregate[task.text].children = childAggregate 
+            }
+        }
+
+        return taskAggregate
+    }
+}
---
+++ taskAggregatorTest.ts
@@ -0,0 +1,84 @@
+import { assertEquals } from "https://deno.land/std@0.210.0/assert/assert_equals.ts";
+import { TaskGraph } from "./models/TaskGraph.ts";
+import { TaskAggregate, TaskAggregator } from "./taskAggregator.ts";
+
+Deno.test('TaskAggregator', async (t) => {
+    const verifyAggregatorResult = (taskGraphsArray: TaskGraph[][], expected: TaskAggregate) => {
+        const aggregator = new TaskAggregator()
+
+        for (const taskGraphs of taskGraphsArray) {
+            aggregator.addTaskGraphs(taskGraphs)
+        }
+
+        assertEquals(aggregator.getResult(), expected)
+    }
+
+    await t.step('aggregates checked and unchecked values', () => {
+        verifyAggregatorResult(
+            [
+                [
+                    {
+                        task: { checked: true, text: 'task 1' },
+                        children: [
+                            { task: { checked: true, text: 'task 1'} },
+                            { task: { checked: true, text: 'task 2'} },
+                            { task: { checked: true, text: 'task 3'} }
+                        ]
+                    },
+                    { task: { checked: true, text: 'task 2' } },
+                    { task: { checked: true, text: 'task 3' } }
+                ],
+                [
+                    {
+                        task: { checked: false, text: 'task 1' },
+                        children: [
+                            { task: { checked: false, text: 'task 1' } },
+                            { task: { checked: false, text: 'task 2' } },
+                            { task: { checked: false, text: 'task 3' } }
+                        ]
+                    },
+                    { task: { checked: false, text: 'task 2' } },
+                    { task: { checked: false, text: 'task 3' } }
+                ],
+                [
+                    {
+                        task: { checked: false, text: 'task 1' },
+                        children: [
+                            { task: { checked: false, text: 'task 1' } },
+                            { task: { checked: true, text: 'task 2' } }
+                        ]
+                    },
+                    { task: { checked: true, text: 'task 2' } },
+                ]
+            ],
+            {
+                'task 1': {
+                    numChecked: 1,
+                    numUnchecked: 2,
+                    children: {
+                        'task 1': {
+                            numChecked: 1,
+                            numUnchecked: 2
+                        },
+                        'task 2': {
+                            numChecked: 2,
+                            numUnchecked: 1
+                        },
+                        'task 3': {
+                            numChecked: 1,
+                            numUnchecked: 1
+                        },
+                    }
+                },
+                'task 2': {
+                    numChecked: 2,
+                    numUnchecked: 1
+                },
+                'task 3': {
+                    numChecked: 1,
+                    numUnchecked: 1
+                }
+            }
+        )
+    })
+})
